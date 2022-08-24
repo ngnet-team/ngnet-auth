92ff32605eb6b8adb2464d95e1f28453cc1ba8f1
Dimitar <Dimitar>
2022-08-24 17:35:10 +0300

    Get rid of StartUp.cs

--- src/Ngnet_Authentication/Web/Program.cs
+++ src/Ngnet_Authentication/Web/Program.cs
@@ -1,20 +1,47 @@
-using Microsoft.AspNetCore.Hosting;
+using ApiModels.Common;
+using Database;
+using Mapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using Web.Infrastructure;
 
-namespace Web
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+            .AddDatabase(builder.Configuration)
+            .AddDbContext<NgnetAuthDbContext>()
+            .AddServices(builder.Configuration)
+            .RegisterFilters(builder.Configuration)
+            .AddControllers();
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseDeveloperExceptionPage();
 }
+
+MappingFactory.GenerateMapper(typeof(ClientErrorModel).GetTypeInfo().Assembly);
+
+app
+    .UseRouting()
+    .UseCors(options => options
+          .AllowAnyOrigin()
+          //.WithOrigins(new string[4]
+          //{
+          //                "http://localhost:3000",
+          //                "http://localhost:4200",
+          //                "http://localhost:5000",
+          //                "http://localhost:5001",
+          //})
+          .AllowAnyHeader()
+          .AllowAnyMethod())
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    })
+    .ApplyMigrations(builder.Configuration);
+
+app.Run();
